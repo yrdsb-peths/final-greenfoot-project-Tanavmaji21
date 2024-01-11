2186e3c6df7e068584026c3d70a7e2125fe71849
Tanavmaji21 <348840182@gapps.yrdsb.ca>
2024-01-10 19:26:16 -0500

    restarting because I feel like idea is to similar to hungry elephant

--- MyWorld.java
+++ MyWorld.java
@@ -21,39 +21,9 @@ public class MyWorld extends World
         super(600, 400, 1);
         
         // Creating a person in the world
-        Person tanav = new Person();
-        addObject(tanav,300,200);
-        
-        createOrange();
-        scoreLabel = new Label(0,70);
-        addObject(scoreLabel, 550,30);
 
     }
 
-     public void gameOver()
-    {
-        Label gameOverLabel = new Label("Game Over", 200);
-        addObject(gameOverLabel, 300, 200);
-    }
-    
-     public void createOrange()
-    {
-       Orange orange = new Orange();
-       orange.setSpeed(level);
-       int x = Greenfoot.getRandomNumber(600);
-       int y = 0;
-       addObject(orange, x, y);
-    }
-    
-    public void increaseScore()
-    {
-        score++;
-        scoreLabel.setValue(score);
-        
-        if(score % 5 == 0)
-        {
-            level += 1;
-        }
-    }
+
     
 }
